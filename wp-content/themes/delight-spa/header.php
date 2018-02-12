d3a15bb851b2d0152a5d5b49c4edaa7ddd89e025
LaurentNougaret <uriel3@hotmail.fr>
2018-02-12 14:52:17 +0100

    added some pages, changed color for navbar

--- wp-content/themes/delight-spa/header.php
+++ wp-content/themes/delight-spa/header.php
@@ -13,7 +13,7 @@
 <meta name="viewport" content="width=device-width">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link href=”https://fonts.googleapis.com/css?family=Raleway” rel=”stylesheet”>
+<link href="https://fonts.googleapis.com/css?family=Lora|Open+Sans|Raleway|Roboto" rel="stylesheet">
 <?php wp_head(); ?>
 
 </head>
